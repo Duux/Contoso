2f5063aa308a42c7748f4a0d94d541b6236a967f
Justin Connors <justinconnors@hotmail.ca>
2019-01-24 10:58:59 -0400

    Instructor

---
+++ ContosoU_2019/Models/CourseAssignment.cs
@@ -0,0 +1,25 @@
+﻿namespace ContosoU_2019.Models
+{
+    public class CourseAssignment
+    {
+        public int InstructorID { get; set; }//Composite PK, FK to Instructor Entity
+
+        public int CourseID { get; set; }//Composite PK, FK to Course Entity
+
+        /*
+         * The only way to identify composite pk to EF is by using the Fluent API
+         * Within the DbContext class (in this case the SchoolContext class)
+         * It cannot be done using attributes
+         */
+
+        //Navigation properties
+        //Many to Many (this is the junction table between courses and instructor)
+        //Many instructors teaching many courses
+        //1 course many Course Assignments
+        //1 instructor many course Assignments\
+
+        public virtual Instructor Instructor { get; set; }
+        public virtual Course Course { get; set; }
+
+    }
+}
--- ContosoU_2019/Models/Instructor.cs
+++ ContosoU_2019/Models/Instructor.cs
@@ -12,5 +12,9 @@ namespace ContosoU_2019.Models
         [DataType(DataType.Date), Display(Name = "Enrollment Date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime HireDate { get; set; }
 
+        //Navigation property
+        //An instructor can teach ny number of courses, so Courses is defined as a collection of the CourseAssignment Entity
+        public virtual ICollection<CourseAssignment> Courses { get; set; }
+
     }
 }

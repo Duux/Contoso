b1e1c2209f7d3c1bbebe226c02ac1ec348b60841
Justin Connors <justinconnors@hotmail.ca>
2019-01-24 11:15:16 -0400

    Context

--- ContosoU_2019/Data/SchoolContext.cs
+++ ContosoU_2019/Data/SchoolContext.cs
@@ -21,6 +21,28 @@ namespace ContosoU_2019.Data
         public DbSet<Instructor> Instructors { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
 
+
+        /*
+         * Plural table names by default:
+         * When the database is created (from migration process), EF creates tables that have
+         * the same names as the DbSet property names.
+         * -> Just a developer debate (plural vs singular table names)
+         */
+
+        //Override the behaviour using Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            //modelBuilder.Entity<Student>().ToTable("Student");
+            //modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<Person>().ToTable("Person");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+
+            //Composite PK on CourseAssignment (CourseID, InstructorID)
+            modelBuilder.Entity<CourseAssignment>().HasKey(c => new { c.CourseID, c.InstructorID });
+        }
     }
 }
